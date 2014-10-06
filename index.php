c6f3dee1137ef8b1ec566b7cac8ab004de9efcc3
Matt Beall <mbeall@starverte.com>
2014-10-06 11:45:51 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Home Page
+ *
+ * This is the "home" page, often the first page
+ * that a user encounters when visiting the website.
+ *
+ * @author  Matt Beall
+ * @package Echo/PHP
+ * @version 0.0.1
+ */
