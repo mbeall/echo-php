fb32465d6a6cb2f40fd55422351fc1ff2453ea83
Matt Beall <mbeall@starverte.com>
2014-10-06 11:49:40 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Header
+ *
+ * Contains the opening <html> and <head> section
+ * along with the header and navigation that is consistent
+ * on all pages.
+ *
+ * @author  Matt Beall
+ * @package Echo/PHP
+ * @version 0.0.1
+ */
