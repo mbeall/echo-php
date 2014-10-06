b2aa28cb5a9d806299dc3ea2038d0aa85e1a8ec1
Matt Beall <mbeall@starverte.com>
2014-10-06 12:05:33 -0600

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Profile page
+ *
+ * If logged in, this page allows user to edit own information.
+ * If not logged in, or different user from current profile,
+ * this page displays public information for a particular user.
+ *
+ * @author  Hannah Turner
+ * @package Echo/PHP
+ * @since 0.0.1
+ *
+ * @todo Validate input fields
+ */
