dede0d36556c41dcd8eea51e9f2589774df71768
Matt Beall <mbeall@starverte.com>
2014-10-06 12:06:13 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * About page
+ *
+ * A page that describes the organization/project/website
+ *
+ * @author  Crystal Carr
+ * @package Echo/PHP
+ * @since 0.0.1
+ *
+ * @todo Validate input fields
+ */
