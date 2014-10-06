c3312e5cae74948c14c2bed2aa11d7783a9ba65a
Matt Beall <mbeall@starverte.com>
2014-10-06 12:01:01 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Login page
+ *
+ * Allows a registered user to login
+ *
+ * @author  Matt Beall
+ * @author  Hannah Turner
+ * @package Echo/PHP
+ * @since 0.0.1
+ *
+ * @todo Validate input fields
+ */
