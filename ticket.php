c9a98b65e5ef5dfad4a5a8084ed4d5ff89c26e65
Matt Beall <mbeall@starverte.com>
2014-10-06 12:02:08 -0600

    Create ticket.php

---
+++ ticket.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Single ticket page
+ *
+ * Allows a ticket to be created or edited
+ *
+ * @author  Matt Beall
+ * @author  Crystal Carr
+ * @package Echo/PHP
+ * @since 0.0.1
+ *
+ * @todo Validate input fields
+ */
