202bb8f846f3fb9a778d08e7da22f4d46ff90ed1
Matt Beall <mbeall@starverte.com>
2014-10-06 12:09:28 -0600

    Create tag.php

---
+++ tag.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Edit tag page
+ *
+ * Allows a registered user to create or edit a tag
+ *
+ * @author  Crystal Carr
+ * @package Echo/PHP
+ * @since   0.0.1
+ *
+ * @todo Validate input fields
+ */
