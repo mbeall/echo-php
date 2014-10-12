11e68a8ca087c94829b7b26037c388c203e90709
Matt Beall <mbeall@starverte.com>
2014-10-12 16:39:20 -0600

    Prepare for merge with master

--- contact.php
+++ contact.php
@@ -24,13 +24,13 @@ include_once ('header.php'); ?>
             </header><!-- .entry-header -->
 
             <div class="entry-content">
-							<p>Please report an issues you find on the Echo website to the development team at <a href="mailto:dev@starverte.com">Star Verte LLC.</a></p>
-							<p>If you are need to submit a ticket regarding a software or hardware issue please create the ticket using the <a href="ticket.php">ticket form.</a></p>
-							<p>To contact the webmasters for any other reasons please contact the Star Verte LLC <a href="mailto:dev@starverte.com">here.</a></p>
-						</div> <!--.entry-content -->
-					</article>
-				</div> <!--.row -->
-			</div> <!-- #content -->
-		</div> <!-- #primary -->
+              <p>Please report an issues you find on the Echo website to the development team at <a href="mailto:dev@starverte.com">Star Verte LLC.</a></p>
+              <p>If you are need to submit a ticket regarding a software or hardware issue please create the ticket using the <a href="ticket.php">ticket form.</a></p>
+              <p>To contact the webmasters for any other reasons please contact the Star Verte LLC <a href="mailto:dev@starverte.com">here.</a></p>
+            </div> <!--.entry-content -->
+          </article>
+        </div> <!--.row -->
+      </div> <!-- #content -->
+    </div> <!-- #primary -->
 
 <?php include_once('footer.php');?>
