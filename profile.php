2a569f61756b7b1d4e50b6a0a9263e2200db89d8
Hmturner <hannahmaeturner@gmail.com>
2014-10-12 15:54:30 -0600

    Started profile page

--- profile.php
+++ profile.php
@@ -12,3 +12,31 @@
  *
  * @todo Validate input fields
  */
+
+Global $the_title;
+$the_title='Profile';
+include_once ('header.php');
+include_once ('?>      
+
+<div id="primary" class="content-area container">
+      <div id="content" class="site-content col-lg-12 col-md-12" role="main">
+        <div class="row">
+          <article class="page type-page status-draft hentry col-lg-12 col-md-12 col-sm-12">
+            <header class="entry-header">
+              <h1 class="entry-title"><?php echo $the_title; ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">   
+
+<section>
+	<h1>My Profile</h1>
+    <?php echo $u_first . $u_last; ?>
+	<h2>My Contact Info</h2>                    
+        <h3>Email:</h3>        
+</section>                
+                
+                
+                
+                
+                
+<?php include_once ('footer.php');
