b54b78d75c278d83b0fa155c6410e70e51f505eb
Matt Beall <mbeall@starverte.com>
2014-10-19 13:38:19 -0600

    Add function is_user_logged_in

--- functions.php
+++ functions.php
@@ -5,6 +5,16 @@
  * All function definitions for the site
  *
  * @author  Matt Beall
- * @package Echo/PHP
  * @version 0.0.1
  */
+function is_user_logged_in() {
+  global $user;
+
+  $u_id_PK = !empty($user)             ? get_user_data( $user , 'u_id_PK' ) : 0;
+  $u_id    = !empty($_SESSION['u_id']) ? $_SESSION['u_id']                  : '';
+
+  if ($u_id_PK == $u_id)
+    return true;
+  else
+    return false;
+}
