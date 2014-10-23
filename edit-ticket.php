8a82e777878d60cf7a27c2b1bd4f3540248fb1fa
Matt Beall <mbeall@starverte.com>
2014-10-23 11:53:43 -0600

    Clean up code and version numbers

--- edit-ticket.php
+++ edit-ticket.php
@@ -5,8 +5,7 @@
  * Allows a ticket to be edited
  *
  * @author  Crystal Carr
- * @package Echo/PHP
- * @since 0.0.1
+ * @since 0.0.5
  *
  * @todo Validate input fields
  */
@@ -17,9 +16,9 @@ $the_title= 'Edit a Ticket';
 
 
 include_once('header.php');?>
-$tkt_name = get_tkt_name($ticket);  
+$tkt_name = get_tkt_name($ticket);
 $tkt_priority = get_tkt_priority($ticket);
-$tkt_desc = get_tkt_desc($ticket); 
+$tkt_desc = get_tkt_desc($ticket);
 $tkt_status = get_tkt_status($ticket);
 $tag_name = get_tag_name($tag);
 
@@ -31,21 +30,21 @@ $tag_name = get_tag_name($tag);
             <header class="entry-header">
               <h1 class="entry-title"><?php echo $the_title; ?></h1>
             </header><!-- .entry-header -->
-                        
-              <div class="entry-content">   
+
+              <div class="entry-content">
                <?php if (is_admin()) { ?>
-		          
-		          <form class="col-xs-6" action="edit-ticket.php" method="post" name="edit_ticket_user" id="edit_ticket_user">
-		                <input type="hidden" name="tkt_id_pk" value="">
-		                <div class="form-group">
-		                  <label for="tkt_name">Ticket Name</label>
-		                  <input class="form-control" type="text" name="tkt_name" id="tkt_name" maxlength="45" value="<?php echo $tkt_name ?>">
-		                </div>
-                   
-		                <div class="form-group">
-		                	<label for="tkt_priority">Ticket Priority</label>
-		                	<input type="text" name="tkt_priority" Pattern ="^[a-zA-Z0-9][\w\s\&,]*[a-zA-Z0-9\!\?\.]$"  maxlength="8" value="<?php echo $tkt_priority; ?>">
-		                </div>
+
+              <form class="col-xs-6" action="edit-ticket.php" method="post" name="edit_ticket_user" id="edit_ticket_user">
+                    <input type="hidden" name="tkt_id_pk" value="">
+                    <div class="form-group">
+                      <label for="tkt_name">Ticket Name</label>
+                      <input class="form-control" type="text" name="tkt_name" id="tkt_name" maxlength="45" value="<?php echo $tkt_name ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="tkt_priority">Ticket Priority</label>
+                      <input type="text" name="tkt_priority" Pattern ="^[a-zA-Z0-9][\w\s\&,]*[a-zA-Z0-9\!\?\.]$"  maxlength="8" value="<?php echo $tkt_priority; ?>">
+                    </div>
                     <div class="form-group">
                       <label for="tkt_status">Ticket Status</label>
                       <select name="tkt_status">
@@ -55,31 +54,31 @@ $tag_name = get_tag_name($tag);
                         <option value="<?php echo $tkt_status ?>" selected="selected"></option>
                       </Select>
                     </div>
-		                <div class="form-group">
-		                  <label for="tkt_desc">Description:</label>
-		                  <textarea class="form-control" value="<?php echo $tkt_desc ?>"></textarea>
-		                </div>
-		                <p><input type="submit" value="Sumbit">
-		              <a href="index.php">Cancel</a>
-		            </p>
-		              </form>
-		
-          		<?php }else {  ?>
-				     
-	              <?php ('Refresh: 5, URL=login.php');
-						echo '<h2> You need to be logged in to Edit a ticket. You are being redirected to the login page in 5 seconds.</h2>';
-						?>
-					
-				
-					<?php	} ?>
-			
-			<?php update_ticket('tkt_name', 'tkt_desc', 'tkt_priority', 'tkt_status','tags') ?>
-				
-              </div> <!--.entry-content --> 
+                    <div class="form-group">
+                      <label for="tkt_desc">Description:</label>
+                      <textarea class="form-control" value="<?php echo $tkt_desc ?>"></textarea>
+                    </div>
+                    <p><input type="submit" value="Sumbit">
+                  <a href="index.php">Cancel</a>
+                </p>
+                  </form>
+
+              <?php }else {  ?>
+
+                <?php ('Refresh: 5, URL=login.php');
+            echo '<h2> You need to be logged in to Edit a ticket. You are being redirected to the login page in 5 seconds.</h2>';
+            ?>
+
+
+          <?php  } ?>
+
+      <?php update_ticket('tkt_name', 'tkt_desc', 'tkt_priority', 'tkt_status','tags') ?>
+
+              </div> <!--.entry-content -->
            </article>
         </div> <!--.row -->
       </div> <!-- #content -->
-    </div> <!-- #primary --> 
+    </div> <!-- #primary -->
 
 
 
